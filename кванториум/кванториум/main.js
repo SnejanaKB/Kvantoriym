aed19b4e4c69f1592bb1200239cc5933a3385eb3
kvantitpd <kvantitpd@gmail.com>
2024-09-20 13:54:09 +0500

    init

--- кванториум/кванториум/main.js
+++ кванториум/кванториум/main.js
@@ -1,15 +1,29 @@
-const data = [
-    {  
-        "name": "Серега",
-        "surname": "Смирнов",
-      
-    }
-]
+// const data = [
+//     {  
+//         "name": "Серега",
+//         "surname": "Смирнов",
 
-console.log(data)
+//     }
+// ]
+
+// console.log(data)
 
 
-const name = document.querySelector(".name_js")
-const surname = document.querySelector(".surname_js")
-name.innerText = data[0].name
-surname.innerText = data[0].surname
+// const name = document.querySelector(".name_js")
+// const surname = document.querySelector(".surname_js")
+// name.innerText = data[0].name
+// surname.innerText = data[0].surname
+
+async function getData() {
+    const result = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json()) // запрос в бд
+    console.log(result);
+    //  render(result); функция для оборажения
+}
+
+function render(res) {
+    const newDiv = document.createElement("div");
+    newDiv.innerText = res.title;
+    document.querySelector("body").append(newDiv);
+}
+
+getData();
